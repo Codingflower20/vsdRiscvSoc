937569d67e960620ab0389412a8c328d3a835757
Codingflower20 <ojaswinii2005@gmail.com>
2025-06-08 23:48:40 +0530

    Task-12 done And updated week-1.md

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 int main() {
-    volatile int a = 10;
-    volatile int b = 20;
-    volatile int c = a + b;  // You will step through this
-    return 0;
+    int main() {
+    volatile int *led = (int*) 0x10012000;
+    *led = 0x1;
+    while (1);
 }
-
+}
